44ff340441c7beaa0310a894a918cae622764633
Goto-star <goyu620@gmail.com>
2024-05-10 12:11:20 +0900

    FizzBuzzプログラムの作成

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+"use strict";
+
+const numbers = [];
+for (let i = 1; i <= 20; i++) {
+  numbers.push(i);
+}
+
+let FizzBuzz = (numbers) => {
+  numbers.forEach((number) => {
+    if (number % 15 === 0) return console.log("FizzBuzz");
+    if (number % 3 === 0) return console.log("Fizz");
+    if (number % 5 === 0) return console.log("Buzz");
+    return console.log(number);
+  });
+};
+
+FizzBuzz(numbers);
