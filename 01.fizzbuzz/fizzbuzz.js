017947b53f9c629cfa4f5502159ccdfbbcfb9c4a
Goto-star <goyu620@gmail.com>
2024-05-15 11:18:34 +0900

    アロー関数の削除

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,22 +1,18 @@
 #!/usr/bin/env node
 
-const fizzBuzz = () => {
-  const numbers = [];
-  for (let i = 1; i <= 20; i++) {
-    numbers.push(i);
+const numbers = [];
+for (let i = 1; i <= 20; i++) {
+  numbers.push(i);
+}
+
+numbers.forEach((number) => {
+  if (number % 3 === 0 && number % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (number % 3 === 0) {
+    console.log("Fizz");
+  } else if (number % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(number);
   }
-
-  numbers.forEach((number) => {
-    if (number % 3 === 0 && number % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (number % 3 === 0) {
-      console.log("Fizz");
-    } else if (number % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(number);
-    }
-  });
-};
-
-fizzBuzz();
+});
