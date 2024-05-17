c08c9ab5cce526428bb53c792e9e470888e6c631
Goto-star <goyu620@gmail.com>
2024-05-17 19:08:48 +0900

    コールバック形式でプログラムを作成

---
+++ 03.asynchronous/error_callback.js
@@ -0,0 +1,36 @@
+import sqlite3 from "sqlite3";
+const { Database } = sqlite3;
+const db = new Database(":memory:");
+
+// エラーありのプログラム(コールバック)
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    insertRecord();
+  },
+);
+
+// レコードの挿入
+const insertRecord = () => {
+  db.run("INSERT INTO book (title) VALUES (?)", "Book1", function (err) {
+    if (err) {
+      console.error(err.message);
+    }
+    getRecord();
+  });
+};
+
+// レコードの取得
+const getRecord = () => {
+  db.get("SELECT id, title FROM book", function (err) {
+    if (err) {
+      console.error(err.message);
+    }
+    deleteTable();
+  });
+};
+
+// テーブルの削除
+const deleteTable = () => {
+  db.run("DROP TABLE books");
+};
---
+++ 03.asynchronous/no_error_callback.js
@@ -0,0 +1,32 @@
+import sqlite3 from "sqlite3";
+const { Database } = sqlite3;
+const db = new Database(":memory:");
+
+// エラーなしのプログラム(コールバック)
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    insertRecord();
+  },
+);
+
+// レコードの挿入
+const insertRecord = () => {
+  db.run("INSERT INTO books (title) VALUES (?)", "Book1", function () {
+    console.log(`${this.lastID}`);
+    getRecord(this.lastID);
+  });
+};
+
+// レコードの取得
+const getRecord = (id) => {
+  db.get("SELECT id, title FROM books WHERE id = ?", [id], function (_, row) {
+    console.log(`ID = ${row.id}, Title = ${row.title}`);
+    deleteTable();
+  });
+};
+
+// レコードの削除
+const deleteTable = () => {
+  db.run("DROP TABLE books");
+};
