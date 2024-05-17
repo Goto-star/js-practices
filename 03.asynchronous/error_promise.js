d12211f710037d645e5fde1765355b77e204af59
Goto-star <goyu620@gmail.com>
2024-05-17 19:15:16 +0900

    Promiseオブジェクトを使用しプログラム・モジュールを作成

---
+++ 03.asynchronous/error_promise.js
@@ -0,0 +1,29 @@
+import { run, get, errorHandling } from "./promise_method_module.js";
+import sqlite3 from "sqlite3";
+const { Database } = sqlite3;
+const db = new Database(":memory:");
+
+// エラーありのプログラム(promise)
+run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => run(db, "INSERT INTO books (title) VALUES (?)", ["Book1"]))
+  .then((lastID) => {
+    console.log(`${lastID}`);
+  })
+  .catch((err) => {
+    errorHandling(err);
+    return Promise.resolve();
+  })
+  .then(() => {
+    return get(db, "SELECT id, title FROM books");
+  })
+  .then((row) => {
+    console.log(`ID = ${row.id}, Title = ${row.title}`);
+  })
+  .catch((err) => {
+    errorHandling(err);
+    return Promise.resolve();
+  })
+  .then(() => run(db, "DROP TABLE books"));
