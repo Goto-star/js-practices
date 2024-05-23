c695f4d2b4cddbf28f02bc7d842e4530485b4a42
Goto-star <goyu620@gmail.com>
2024-05-23 19:13:26 +0900

    Top-level awaitの使用

--- 03.asynchronous/no_error_async_await.js
+++ 03.asynchronous/no_error_async_await.js
@@ -4,16 +4,29 @@ import { run, get } from "./sqlite_async_utils.js";
 const db = new sqlite3.Database(":memory:");
 
 // エラーなしのプログラム(async/await)
-const main = async () => {
+// const main = async () => {
+//   await run(
+//     db,
+//     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+//   );
+//   const lastID = await run(db, "INSERT INTO books (title) VALUES (?)", "Book1");
+//   console.log(lastID);
+//   const row = await get(db, "SELECT id, title FROM books WHERE id = ?", lastID);
+//   console.log(`ID = ${row.id}, Title = ${row.title}`);
+//   await run(db, "DROP TABLE books");
+// };
+
+try {
   await run(
     db,
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   );
+
   const lastID = await run(db, "INSERT INTO books (title) VALUES (?)", "Book1");
   console.log(lastID);
+
   const row = await get(db, "SELECT id, title FROM books WHERE id = ?", lastID);
   console.log(`ID = ${row.id}, Title = ${row.title}`);
+} finally {
   await run(db, "DROP TABLE books");
-};
-
-main();
+}
