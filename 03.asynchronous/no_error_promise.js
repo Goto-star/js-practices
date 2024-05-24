769642969412a8459e4ced8fe305339873aefb9b
Goto-star <goyu620@gmail.com>
2024-05-24 12:30:01 +0900

    エラーありとエラーなしで受け取る引数名の一貫性を担保するように修正

--- 03.asynchronous/no_error_promise.js
+++ 03.asynchronous/no_error_promise.js
@@ -8,9 +8,9 @@ run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
   .then(() => run(db, "INSERT INTO books (title) VALUES (?)", "Book1"))
-  .then((lastID) => {
-    console.log(lastID);
-    return get(db, "SELECT id, title FROM books WHERE id = ?", lastID);
+  .then((insertResult) => {
+    console.log(insertResult.id);
+    return get(db, "SELECT id, title FROM books WHERE id = ?", insertResult.id);
   })
   .then((row) => {
     console.log(`ID = ${row.id}, Title = ${row.title}`);
