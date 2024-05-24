50831463d16a03cf9c428cff3841ac5e9118705f
Goto-star <goyu620@gmail.com>
2024-05-24 15:48:08 +0900

    エラーが発生するようにクエリを修正

--- 03.asynchronous/error_callback.js
+++ 03.asynchronous/error_callback.js
@@ -5,13 +5,13 @@ const db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
-    db.run("INSERT INTO books (title) VALUES (?)", "Book1", function (err) {
+    db.run("INSERT INTO book (title) VALUES (?)", "Book1", function (err) {
       if (err) {
         console.error(err.message);
       } else {
         console.log(this.lastID);
       }
-      db.get("SELECT id, title FROM books", (err, row) => {
+      db.get("SELECT iid, title FROM books", (err, row) => {
         if (err) {
           console.error(err.message);
         } else {
