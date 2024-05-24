7da9aa8dba97e0c508db59786f5879869c27b432
Goto-star <goyu620@gmail.com>
2024-05-24 13:26:13 +0900

    async_awaitプログラムのロジック周りの修正

--- 03.asynchronous/error_async_await.js
+++ 03.asynchronous/error_async_await.js
@@ -2,37 +2,32 @@ import sqlite3 from "sqlite3";
 import { run, get } from "./sqlite_async_utils.js";
 
 const db = new sqlite3.Database(":memory:");
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-const main = async () => {
-  await run(
+try {
+  const insertResult = await run(
     db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    "INSERT INTO books (title) VALUES (?)",
+    "Book1",
   );
-
-  try {
-    const insertResult = await run(
-      db,
-      "INSERT INTO books (title) VALUES (?)",
-      "Book1",
-    );
-    await run(db, "INSERT INTO books (title) VALUES (?)", "Book1");
-    console.log(insertResult.id);
-  } catch (err) {
-    if (err.message.includes("UNIQUE constraint failed")) {
-      console.log(err.message);
-    }
+  await run(db, "INSERT INTO books (title) VALUES (?)", "Book1");
+  console.log(insertResult.id);
+} catch (err) {
+  if (err.message.includes("UNIQUE constraint failed")) {
+    console.log(err.message);
   }
+}
 
-  try {
-    const row = await get(db, "SELECT id, title FROM book");
-    console.log(`ID = ${row.id}, Title = ${row.title}`);
-  } catch (err) {
-    if (err.message.includes("no such table")) {
-      console.log(err.message);
-    }
+try {
+  const row = await get(db, "SELECT id, title FROM book");
+  console.log(`ID = ${row.id}, Title = ${row.title}`);
+} catch (err) {
+  if (err.message.includes("no such table")) {
+    console.log(err.message);
   }
+}
 
-  await run(db, "DROP TABLE books");
-};
-
-main();
+await run(db, "DROP TABLE books");
--- 03.asynchronous/no_error_async_await.js
+++ 03.asynchronous/no_error_async_await.js
@@ -3,17 +3,22 @@ import { run, get } from "./sqlite_async_utils.js";
 
 const db = new sqlite3.Database(":memory:");
 
-try {
-  await run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-  const lastID = await run(db, "INSERT INTO books (title) VALUES (?)", "Book1");
-  console.log(lastID);
+const insertResult = await run(
+  db,
+  "INSERT INTO books (title) VALUES (?)",
+  "Book1",
+);
+console.log(insertResult.id);
 
-  const row = await get(db, "SELECT id, title FROM books WHERE id = ?", lastID);
-  console.log(`ID = ${row.id}, Title = ${row.title}`);
-} finally {
-  await run(db, "DROP TABLE books");
-}
+const row = await get(
+  db,
+  "SELECT id, title FROM books WHERE id = ?",
+  insertResult.id,
+);
+console.log(`ID = ${row.id}, Title = ${row.title}`);
+await run(db, "DROP TABLE books");
