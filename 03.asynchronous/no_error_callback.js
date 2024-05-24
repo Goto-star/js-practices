a15c4425330972a1954f30746c2f6aa49dd105a0
Goto-star <goyu620@gmail.com>
2024-05-24 12:40:03 +0900

    不要なコメントアウトの削除

--- 03.asynchronous/no_error_callback.js
+++ 03.asynchronous/no_error_callback.js
@@ -2,31 +2,6 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-// db.run(
-//   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-//   () => {
-//     insertAndGetRecord();
-//   },
-// );
-
-// const insertAndGetRecord = () => {
-//   db.run("INSERT INTO books (title) VALUES (?)", "Book1", function () {
-//     console.log(this.lastID);
-//     getRecordAndDeleteTable(this.lastID);
-//   });
-// };
-
-// const getRecordAndDeleteTable = (id) => {
-//   db.get("SELECT id, title FROM books WHERE id = ?", id, (_, row) => {
-//     console.log(`ID = ${row.id}, Title = ${row.title}`);
-//     deleteTable();
-//   });
-// };
-
-// const deleteTable = () => {
-//   db.run("DROP TABLE books");
-// };
-
 db.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
