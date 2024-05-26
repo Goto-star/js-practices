dd42ba4e0d19577e53887507943c89d72b380769
Goto-star <goyu620@gmail.com>
2024-05-26 18:57:55 +0900

    例外を握りつぶさないように修正

--- 03.asynchronous/error_async_await.js
+++ 03.asynchronous/error_async_await.js
@@ -12,12 +12,12 @@ try {
   const result = await run(db, "INSERT INTO book (title) VALUES (?)", "Book1");
   console.log(result.lastID);
 } catch (err) {
-  if (!(err instanceof Error)) {
-    console.log("エラーオブジェクトではありません");
-  } else if (err.message.includes("no such table")) {
-    console.log(err.message);
+  if (err instanceof Error && err.message.includes("no such table")) {
+    console.error(err.message);
+  } else if (err instanceof Error) {
+    console.error("予期しないエラーが発生しました:", err.message, err.stack);
   } else {
-    console.log("エラーが発生しました");
+    console.error("エラーオブジェクトではありません:", err);
   }
 }
 
@@ -25,12 +25,12 @@ try {
   const row = await get(db, "SELECT iid, title FROM books");
   console.log(`ID = ${row.id}, Title = ${row.title}`);
 } catch (err) {
-  if (!(err instanceof Error)) {
-    console.log("エラーオブジェクトではありません");
-  } else if (err.message.includes("no such column")) {
-    console.log(err.message);
+  if (err instanceof Error && err.message.includes("no such column")) {
+    console.error(err.message);
+  } else if (err instanceof Error) {
+    console.error("予期しないエラーが発生しました:", err.message, err.stack);
   } else {
-    console.log("エラーが発生しました");
+    console.error("エラーオブジェクトではありません:", err);
   }
 }
 
