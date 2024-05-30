ca72a57dd9116dc4023a52d2242654288c808ace
Goto-star <goyu620@gmail.com>
2024-05-30 10:44:51 +0900

    error_async_await形式のプログラミングのコード修正

--- 03.asynchronous/error_async_await.js
+++ 03.asynchronous/error_async_await.js
@@ -3,43 +3,31 @@ import { run, get } from "./sqlite_async_utils.js";
 
 const db = new sqlite3.Database(":memory:");
 
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+
 try {
-  await run(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-
-  try {
-    const result = await run(
-      db,
-      "INSERT INTO book (title) VALUES (?)",
-      "Book1",
-    );
-    console.log(result.lastID);
-  } catch (err) {
-    if (err && err.message && err.message.includes("no such table")) {
-      console.error(err.message);
-    } else {
-      throw err;
-    }
-  }
-
-  try {
-    const row = await get(db, "SELECT iid, title FROM books");
-    console.log(`ID = ${row.id}, Title = ${row.title}`);
-  } catch (err) {
-    if (err && err.message && err.message.includes("no such column")) {
-      console.error(err.message);
-    } else {
-      throw err;
-    }
-  }
+  const result = await run(db, "INSERT INTO book (title) VALUES (?)", "Book1");
+  console.log(result.lastID);
 } catch (err) {
-  if (err instanceof Error) {
-    console.error("エラーが発生しました: ", err.message);
+  if (err?.message?.match(/^SQLITE_ERROR: no such table/)) {
+    console.error(err.message);
   } else {
-    console.error("予期しないエラーが発生しました: ", err);
+    throw err;
   }
-} finally {
-  await run(db, "DROP TABLE books");
 }
+
+try {
+  const row = await get(db, "SELECT iid, title FROM books");
+  console.log(`ID = ${row.id}, Title = ${row.title}`);
+} catch (err) {
+  if (err?.message?.match(/^SQLITE_ERROR: no such column/)) {
+    console.error(err.message);
+  } else {
+    throw err;
+  }
+}
+
+await run(db, "DROP TABLE books");
